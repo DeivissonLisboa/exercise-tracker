447a6b8f8e5a92c85a7476ce6080a59f3795e7b7
Deivisson Lisboa <dvilaflor@gmail.com>
2023-06-26 18:26:45 -0300

    Create userSchema and /api/users endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const bodyParser = require("body-parser")
 require("dotenv").config()
 const MONGO_URI = process.env["MONGO_URI"]
 const mongoose = require("mongoose")
@@ -12,12 +13,36 @@ mongoose.connect(MONGO_URI, {
   useUnifiedTopology: true,
 })
 
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+})
+
+const User = mongoose.model("user", userSchema)
+
 // Express
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html")
+})
+
+app.post("/api/users", async (req, res) => {
+  const username = req.body.username
+
+  let result = await User.create({ username })
+
+  res.json(result)
+})
+
+app.get("/api/users", async (req, res) => {
+  const users = await User.find()
+
+  res.json(users)
 })
 
 mongoose.connection.once("open", (err) => {
