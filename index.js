fa9a47bc6c425c306cb7ac0f2e6b349f7d24eb20
Deivisson Lisboa <dvilaflor@gmail.com>
2023-06-26 18:12:21 -0300

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors")
+require("dotenv").config()
+const MONGO_URI = process.env["MONGO_URI"]
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
+// MongoDB
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// Express
 app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+app.use(express.static("public"))
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
 
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+mongoose.connection.once("open", (err) => {
+  if (err) return console.error(err)
+  console.log("Connected to MongoDB")
+  const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("Your app is listening on port " + listener.address().port)
+  })
 })
